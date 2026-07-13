118f70727939cb158df42752e789dcbe21371c65
Priyank Hajela <35231175+psonH@users.noreply.github.com>
2026-07-12 23:22:14 -0400

    astro config ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
   integrations: [react()],
+  adapter: vercel(),
 });
